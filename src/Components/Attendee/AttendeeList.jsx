b78537879d61a906a887e0ee1da740c71214204b
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-11-30 01:13:18 +0200

    feat: :lipstick: structure and styles change

--- src/Components/Attendee/AttendeeList.jsx
+++ src/Components/Attendee/AttendeeList.jsx
@@ -1,43 +1,21 @@
-export const AttendeeList = () => {
+
+export const Attendees = (props) => {
   return (
-    <>
-      <h2 className="tableName py-4">Attendee list</h2>
-
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">First Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Last Name</th>
-            <th scope="col" colSpan="2">
-              Age
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">Augustina</th>
-            <td>august@gmail.com</td>
-            <td>Drogba</td>
-            <td>28</td>
-            <td>
-              <button className="col btn-edit"></button>
-              <button className="col btn-delete"></button>
-            </td>
-          </tr>
-
-          <tr>
-            <th scope="row">Benas</th>
-            <td>benu@gmail.com</td>
-            <td>Juozapavicius</td>
-            <td>12</td>
-            <td>
-              <button className="col btn-edit"></button>
-              <button className="col btn-delete"></button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+    <table className="table">
+      <tbody className="">
+        <tr className="row">
+          <td className="col">{props.info.firstName}</td>
+          <td className="col">{props.info.email}</td>
+          <td className="col">{props.info.lastName}</td>
+          <td className="col">{props.info.age}</td>
+          <td className="col">
+            <button
+              onClick={() => props.onDelete(props.info.id)}
+              className="col button-delete "
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
