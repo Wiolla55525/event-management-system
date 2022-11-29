ba6a4629a2ff60921a0b87d712b22bb74c09157b
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-11-30 00:54:53 +0200

    feat: :sparkles: AttendeeForm onChange functionality initialised

--- src/Components/Attendee/AddAttendee.jsx
+++ src/Components/Attendee/AddAttendee.jsx
@@ -1,35 +1,76 @@
+import { useState } from "react";
 import "./form.css";
 
-export const AddAttendee = () => {
+export const AddAttendeeForm = (props) => {
+  const initialValueState = props.changeValueOfState
+    ? props.changeValueOfState
+    : { firstName: "", email: "", lastName: "", age: "" };
+
+  const [value, setForm] = useState({ ...initialValueState });
+
+  function formChangeHandler(e) {
+    setForm({ ...value, [e.target.id]: e.target.value });
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    props.handleAddAttendee(value);
+    setForm({ ...initialValueState });
+  }
+
   return (
     <div className="Container my-4">
-      <h2 className="tableName py-3">Create Attendee</h2>
-
-      <form>
-        <div className="row">
+      <form onSubmit={onSubmit}>
+        <div className="row align-items-center">
           <div className="col">
             <input
+              className="form-control "
               type="text"
-              className="form-control"
-              placeholder="First name"
+              id="firstName"
+              value={value.firstName}
+              onChange={formChangeHandler}
+              placeholder="Friends name"
+              required
             />
           </div>
           <div className="col">
-            <input type="email" className="form-control" placeholder="Email" />
-          </div>
-          <div className="col">
             <input
+              className="form-control "
               type="text"
-              className="form-control"
-              placeholder="Last name"
+              id="email"
+              onChange={formChangeHandler}
+              value={value.email}
+              required
+              placeholder="E-mail"
             />
           </div>
           <div className="col">
-            <input type="text" className="form-control" placeholder="Age" />
+            <input
+              className="form-control "
+              type="text"
+              id="lastName"
+              onChange={formChangeHandler}
+              value={value.lastName}
+              required
+              placeholder="Last Name"
+            />
           </div>
-          <button type="submit" value="Submit" className="col SubmitButton">
-            Submit
-          </button>
+          <div className="col">
+            <input
+              className="form-control "
+              id="age"
+              type="number"
+              onChange={formChangeHandler}
+              value={value.age}
+              required
+              placeholder="Age"
+            />
+          </div>
+          <div className="col">
+            <button type="submit" value="Submit" className=" SubmitButton">
+              {props.prop}
+            </button>
+          </div>
         </div>
       </form>
     </div>
