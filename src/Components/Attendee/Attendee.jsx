91b3905010e58a99ae81c5330f21c2aa582729e6
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-11-30 01:01:11 +0200

    perf: :construction: Attendee fn removed Dashboard element
    no attendees view

--- src/Components/Attendee/Attendee.jsx
+++ src/Components/Attendee/Attendee.jsx
@@ -1,14 +1,35 @@
-import { AddAttendee } from "./AddAttendee";
+import { AddAttendeeForm } from "./AddAttendee";
 import Container from "react-bootstrap/Container";
 import { Dashboard } from "../Dashboard/Dashboard";
-import { AttendeeList } from "./AttendeeList";
+import { Attendees } from "./AttendeeList";
+import { useState, useEffect } from "react";
+
 
 export function Attendee() {
+  const [attendees, setAttendees] = useState([]);
+
   return (
-    <Container>
-      <Dashboard />
-      <AddAttendee />
-      <AttendeeList/>
+
+<Container>
+      <div className="pt-5 pb-2 text-center">
+        <h1 className="">
+          MANAGEMENT <br /> DASHBOARD
+        </h1>
+      </div>
+
+      <div className=" row d-flex justify-content-center">
+        <div className="mb-5 col Container-bigger">
+          <h2 className="">Create Attendee</h2>
+          <AddAttendeeForm
+            prop="Submit"
+          />
+        </div>
+      </div>
+
+      <div className=" row d-flex justify-content-center">
+      <h2 className="tableName py-4">Attendee list:</h2>
+        {attendees.length > 0 ? attendees : (<div className="text-center">No attendees found</div>)}
+      </div>
     </Container>
   );
 }
