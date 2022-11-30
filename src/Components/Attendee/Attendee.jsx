357b74b40b2e1c98274c74a25c49df1e00be5fc9
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-11-30 16:40:09 +0200

    feat: :sparkles: adding Attendee and handelingDelete implemented in local storage

--- src/Components/Attendee/Attendee.jsx
+++ src/Components/Attendee/Attendee.jsx
@@ -1,16 +1,49 @@
+import { Attendees } from "./AttendeeList";
 import { AddAttendeeForm } from "./AddAttendee";
 import Container from "react-bootstrap/Container";
-import { Dashboard } from "../Dashboard/Dashboard";
-import { Attendees } from "./AttendeeList";
 import { useState, useEffect } from "react";
 
-
 export function Attendee() {
-  const [attendees, setAttendees] = useState([]);
+  function getSavedData() {
+    const savedData = localStorage.getItem("Attendee");
+    const res = JSON.parse(savedData);
+    return res || [];
+  }
+
+  const [attendees, setAttendees] = useState(getSavedData());
+
+  useEffect(() => {
+    localStorage.setItem("Attendee", JSON.stringify(attendees));
+  }, [attendees]);
+
+  function handleDeleteAttendee(id) {
+    setAttendees(attendees.filter((attendee) => attendee.id !== id));
+  }
+
+  function handleAddAttendee(data, id) {
+    setAttendees([
+      ...attendees,
+      {
+        ...data,
+        id: new Date().getTime(),
+      },
+    ]);
+  }
+
+
+
+  const addAttendee = attendees.map((attendee) => (
+    <div key={attendee.id} className=" my-1 align">
+      <Attendees
+        key={attendee.id}
+        info={attendee}
+        onDelete={handleDeleteAttendee}
+      />
+    </div>
+  ));
 
   return (
-
-<Container>
+    <Container>
       <div className="pt-5 pb-2 text-center">
         <h1 className="">
           MANAGEMENT <br /> DASHBOARD
@@ -22,13 +55,18 @@ export function Attendee() {
           <h2 className="">Create Attendee</h2>
           <AddAttendeeForm
             prop="Submit"
+            handleAddAttendee={(value, id) => handleAddAttendee(value, id)}
           />
         </div>
       </div>
 
       <div className=" row d-flex justify-content-center">
-      <h2 className="tableName py-4">Attendee list:</h2>
-        {attendees.length > 0 ? attendees : (<div className="text-center">No attendees found</div>)}
+        <h2 className="tableName py-4">Attendee list:</h2>
+        {attendees.length > 0 ? (
+          addAttendee
+        ) : (
+          <div className="text-center">No attendees found</div>
+        )}
       </div>
     </Container>
   );
